6e83a00a26c12c34d3c0e26447a51100329bd244
mahsaprs84925 <mahsa.pooresmaeil1384925@gmail.com>
2025-04-23 23:45:47 +0430

    add is done

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -6,14 +6,14 @@ public class Game {
     private int price;
 
     public Game(String name, double rating, int price) {
-        //TODO
-
+        this.name = name;
+        this.rating = rating;
+        this.price = price;
     }
 
     public String getName() {
         return name;
     }
-
 
     public double getRating() {
         return rating;
@@ -25,8 +25,7 @@ public class Game {
 
     @Override
     public String toString() {
-        //TODO
-        return "";
+        return String.format("%s - Rating: %.1f/5 - Price: %d€", name, rating, price);
     }
 
     @Override
@@ -34,7 +33,8 @@ public class Game {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Game game = (Game) o;
-        return Double.compare(game.rating, rating) == 0 && Double.compare(game.price, price) == 0 && Objects.equals(name, game.name);
+        return Double.compare(game.rating, rating) == 0 &&
+                price == game.price &&
+                Objects.equals(name, game.name);
     }
-
 }
